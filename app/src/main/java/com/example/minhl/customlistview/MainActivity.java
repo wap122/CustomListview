04a45b9f9a93c4155d9ae6479375b159683ad495
MinhSacred <minhlevan104@gmail.com>
2017-06-27 08:48:57 +0700

    New Change

--- app/src/main/java/com/example/minhl/customlistview/MainActivity.java
+++ app/src/main/java/com/example/minhl/customlistview/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.minhl.customlistview;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
